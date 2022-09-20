61d616cb8ba49d46f1d3d27da1eed0d813fe96f6
Elif İrem Külcü <78558459+elffirem@users.noreply.github.com>
2022-09-20 10:44:10 +0300

    Exercise 8 added

---
+++ Exercise8.sql
@@ -0,0 +1,67 @@
+--create tableCREATE TABLE employee(
+	id INTEGER,
+	name VARCHAR(50),
+	brithday DATE,
+	email VARCHAR(100)
+
+)
+--add data to table
+insert into employee (id, name, brithday, email) values (1, 'Denise Scourge', '1970-12-03', 'dscourge0@meetup.com');
+insert into employee (id, name, brithday, email) values (2, 'Justinian Voaden', '1985-10-21', 'jvoaden1@whitehouse.gov');
+insert into employee (id, name, brithday, email) values (3, 'Pennie Pembry', '1918-12-27', 'ppembry2@vistaprint.com');
+insert into employee (id, name, brithday, email) values (4, 'Nikki Parrot', '2000-03-27', 'nparrot3@exblog.jp');
+insert into employee (id, name, brithday, email) values (5, 'Noe Mariolle', '1965-01-29', 'nmariolle4@washington.edu');
+insert into employee (id, name, brithday, email) values (6, 'Lilli Shuttleworth', '1974-01-17', 'lshuttleworth5@dion.ne.jp');
+insert into employee (id, name, brithday, email) values (7, 'Meredithe Finlater', '1911-04-15', 'mfinlater6@bbc.co.uk');
+insert into employee (id, name, brithday, email) values (8, 'Everett Thomerson', '1971-12-05', 'ethomerson7@amazon.co.uk');
+insert into employee (id, name, brithday, email) values (9, 'Jone Leathley', '1944-08-23', 'jleathley8@behance.net');
+insert into employee (id, name, brithday, email) values (10, 'Blinni Bradder', '1906-07-24', 'bbradder9@omniture.com');
+insert into employee (id, name, brithday, email) values (11, 'Katharina Euels', '1982-06-21', 'keuelsa@hao123.com');
+insert into employee (id, name, brithday, email) values (12, 'Gaby Matignon', '1945-12-12', 'gmatignonb@yahoo.co.jp');
+insert into employee (id, name, brithday, email) values (13, 'Salome Scrowson', '1979-11-29', 'sscrowsonc@seesaa.net');
+insert into employee (id, name, brithday, email) values (14, 'Dicky Coonan', '1971-01-25', 'dcoonand@sfgate.com');
+insert into employee (id, name, brithday, email) values (15, 'Jacquelynn Gregoratti', '1960-12-28', 'jgregorattie@tripod.com');
+insert into employee (id, name, brithday, email) values (16, 'Paolina Keech', '1959-07-12', 'pkeechf@techcrunch.com');
+insert into employee (id, name, brithday, email) values (17, 'Bailie Gasgarth', '1913-09-16', 'bgasgarthg@icq.com');
+insert into employee (id, name, brithday, email) values (18, 'Rodrigo Alvarez', '1973-05-15', 'ralvarezh@amazon.co.jp');
+insert into employee (id, name, brithday, email) values (19, 'Eduard Beardshaw', '1989-06-11', 'ebeardshawi@phpbb.com');
+insert into employee (id, name, brithday, email) values (20, 'Callie Baddeley', '1905-01-20', 'cbaddeleyj@nydailynews.com');
+insert into employee (id, name, brithday, email) values (21, 'Tildie Cordel', '1980-02-06', 'tcordelk@rakuten.co.jp');
+insert into employee (id, name, brithday, email) values (22, 'Sybila Rodolico', '1931-01-26', 'srodolicol@parallels.com');
+insert into employee (id, name, brithday, email) values (23, 'Tedie Semple', '1975-06-26', 'tsemplem@ow.ly');
+insert into employee (id, name, brithday, email) values (24, 'Cornelius Barrington', '1978-04-29', 'cbarringtonn@technorati.com');
+insert into employee (id, name, brithday, email) values (25, 'Anderea Ceeley', '1913-01-22', 'aceeleyo@abc.net.au');
+insert into employee (id, name, brithday, email) values (26, 'Xylia Busek', '1939-02-23', 'xbusekp@elegantthemes.com');
+insert into employee (id, name, brithday, email) values (27, 'Lola Sayes', '1947-01-26', 'lsayesq@blogtalkradio.com');
+insert into employee (id, name, brithday, email) values (28, 'Ralina Parffrey', '1952-09-17', 'rparffreyr@foxnews.com');
+insert into employee (id, name, brithday, email) values (29, 'Gisela Chillingworth', '1958-05-24', 'gchillingworths@indiatimes.com');
+insert into employee (id, name, brithday, email) values (30, 'Bordy Bracknell', '1978-06-23', 'bbracknellt@com.com');
+insert into employee (id, name, brithday, email) values (31, 'Shaine Southcomb', '1981-12-02', 'ssouthcombu@sakura.ne.jp');
+insert into employee (id, name, brithday, email) values (32, 'Clyde Leifer', '1947-10-27', 'cleiferv@gizmodo.com');
+insert into employee (id, name, brithday, email) values (33, 'Wendall Hugenin', '1958-08-05', 'whugeninw@uiuc.edu');
+insert into employee (id, name, brithday, email) values (34, 'Yank Bescoby', '1906-06-01', 'ybescobyx@phpbb.com');
+insert into employee (id, name, brithday, email) values (35, 'Eleanore Shwalbe', '1952-01-28', 'eshwalbey@wiley.com');
+insert into employee (id, name, brithday, email) values (36, 'Britta Newens', '1916-02-26', 'bnewensz@unc.edu');
+insert into employee (id, name, brithday, email) values (37, 'Steffane Jonsson', '1902-01-20', 'sjonsson10@1und1.de');
+insert into employee (id, name, brithday, email) values (38, 'Leeann Bentke', '1980-08-06', 'lbentke11@hud.gov');
+insert into employee (id, name, brithday, email) values (39, 'Edward Skewis', '1961-03-15', 'eskewis12@amazon.com');
+insert into employee (id, name, brithday, email) values (40, 'Dick Pettitt', '1927-02-26', 'dpettitt13@gnu.org');
+insert into employee (id, name, brithday, email) values (41, 'Elita Baskeyfied', '1956-06-29', 'ebaskeyfied14@dell.com');
+insert into employee (id, name, brithday, email) values (42, 'Malachi Jansson', '1973-01-20', 'mjansson15@ask.com');
+insert into employee (id, name, brithday, email) values (43, 'Packston Gerrelts', '1965-09-16', 'pgerrelts16@macromedia.com');
+insert into employee (id, name, brithday, email) values (44, 'Dionis Meneer', '1955-12-20', 'dmeneer17@nps.gov');
+insert into employee (id, name, brithday, email) values (45, 'Hayyim Snadden', '1907-12-07', 'hsnadden18@jalbum.net');
+insert into employee (id, name, brithday, email) values (46, 'Sam Shemilt', '1951-09-24', 'sshemilt19@cmu.edu');
+insert into employee (id, name, brithday, email) values (47, 'Adelice Skelhorne', '1967-01-21', 'askelhorne1a@marriott.com');
+insert into employee (id, name, brithday, email) values (48, 'Brock Ollett', '1989-05-25', 'bollett1b@ustream.tv');
+insert into employee (id, name, brithday, email) values (49, 'Wilton McSporon', '1952-10-16', 'wmcsporon1c@ehow.com');
+insert into employee (id, name, brithday, email) values (50, 'Christie O''Cleary', '1989-06-02', 'cocleary1d@springer.com');
+
+--update table 
+UPDATE employee
+	SET name='UPDATED' 
+	WHERE id IN(1,2,3,4,5)
+	
+--delete from table
+DELETE FROM employee
+WHERE name='UPDATED'
